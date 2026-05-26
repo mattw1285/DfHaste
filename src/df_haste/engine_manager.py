ae14b39a50877ff237daf2abd5032f0ae7578730
Matt Wheeler <mattw1285@gmail.com>
2026-05-26 21:13:21 +0100

    minor start to engine manager

--- src/df_haste/engine_manager.py
+++ src/df_haste/engine_manager.py
@@ -1,6 +1,16 @@
 from sqlalchemy import create_engine
 
 class EngineManager:
-    pass
+    """ A thin wrap on sqlalcehmy!
 
-print('test')
+    The aim is to make working in an env with lots of db connections much more convenient
+    """
+    def __init__(self, config='sample_location'):
+        self._engines = {}
+
+    def get_engine(''):
+        pass
+
+    def dispose():
+        pass
+
