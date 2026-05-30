e98130ab0b43e9914ac60cced53fa27cc050896f
Matt Wheeler <mattw1285@gmail.com>
2026-05-30 19:52:08 +0100

    added path config reader

--- src/df_haste/config_handler.py
+++ src/df_haste/config_handler.py
@@ -1,9 +1,13 @@
 ## Standard Library Imports ##
 import tomllib
 from pathlib import Path
+from typing import Self
 
 ## Third Party Imports ##
-from pydantic import BaseModel
+from pydantic import (
+    BaseModel,
+    model_validator
+)
 
 class DatabaseConfig(BaseModel):
     """ Pydantic BaseModel to encapsulate connection details 
@@ -12,32 +16,61 @@ class DatabaseConfig(BaseModel):
     """
     name: str
     driver: str
-    
+    hostname: str|None = None
+    sid: str|None = None
+    service_name: str|None = None
+    path: Path|None = None
+    username: str|None = None
+    password: str|None = None
+
+    @model_validator(mode='after')
+    def validate_path(self) -> Self:
+        if self.driver == 'sqlite3' and self.path is None:
+            raise ValueError(f"{self.name} (sqlite3) needs a 'path'")
+        elif self.driver != 'sqlite3' and self.path is not None:
+            raise ValueError(f"only (sqlite3) needs a 'path' not {self.name}")
+        return self
+
+    @model_validator(mode='after')
+    def validate_host(self) -> Self:
+        pass
+
+    @model_validator(mode='after')
+    def validate_auth(self) -> Self:
+        pass
+
+       
 class SqlQuery(BaseModel):
-    path: Path
+    """ Pydantic BaseModel to cover saved queries metadata
+
+    ## Write Me! ##
+    """
+    pass
+
 
 class ConfigHandler():
     """ Parent class to handle pydantic components.
 
     ## Write Me! ##
     """
-    def __init__(self):
-        with open('config.toml', "rb") as f:
+    @classmethod
+    def _read_config(cls) -> dict:
+        config_path = Path(__file__).parent / 'config.toml'
+        with open(config_path, "rb") as f:
             data = tomllib.load(f)
-        data = data['database']
-        d = {}
-        for x in data:
-            if x['name'] in d: 
-                raise ValueError
-            d[x['name']] = x 
-            del d[x['name']]['name']
-        print(d)
-
-    def databases(self) -> list[DatabaseConfig]:
+        return data
+ 
+    @classmethod
+    def databases(cls) -> list[DatabaseConfig]:
         """ Responsible for serving database config objects. """
         pass
     
-    def sql_queries()
+    @classmethod
+    def sql_queries(cls) -> list[SqlQuery]:
         pass
-    
-    
+
+    @classmethod
+    def get_database(self, name:str) -> DatabaseConfig:
+        pass
+
+print(ConfigHandler._read_config())
